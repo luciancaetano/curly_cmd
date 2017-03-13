26641822a5ac434b7a7ebfac8c2646e6132b91f8
Lucian <suporte@suporte-PC.(none)>
2017-03-13 14:42:57 -0300

    Tiny updates

--- electron-package/entry.js
+++ electron-package/entry.js
@@ -5,8 +5,8 @@ const app = electron.app;
 
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
-process.env.NODE_ENV = 'development';
-//process.env.NODE_ENV = 'production';
+//process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 const path = require('path');
 const url = require('url');
 
